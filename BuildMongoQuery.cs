28d952edc248530fc4c811ce2ddb0689a504be26
Santosh Singh <santosh.ksingh03@gmail.com>
2021-10-08 17:52:06 +0800

    ab

---
+++ BuildMongoQuery.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace QueryLanguage
+{
+    public class BuildMongoQuery2
+    {
+
+        //build query from stack
+
+        public static string BuildMongoQuery(Stack<object> elements)
+        {
+            
+
+           
+            var where = elements?.Pop();
+            var find = elements?.Pop();
+            var select = elements?.Pop();
+
+            return find + ".find(" + JsonConvert.SerializeObject(where) + "," + JsonConvert.SerializeObject(select) + ")";
+
+
+        }
+    }
+    public class BuildMongoQuery
+    {
+        private Stack<object> _elements;
+
+        public BuildMongoQuery(Stack<object> elements)
+        {
+            _elements = elements;
+
+        }
+        public object BuildBinary(object op, object left, object right)
+        {
+
+            return new Dictionary<object, object> {
+                {
+                    op,
+                    new List<object> {left,right }
+                    }
+                };
+        }
+        public object BuildSelect(object field)
+        {
+
+
+            return field;
+
+        }
+        public object BuildFrom(object op)
+        {
+            return "db." + op;
+
+        }
+        public void Parse(string op)
+        {
+
+            if (op == "$and")
+            {
+                _elements.Push(BuildBinary(op, _elements.Pop(), _elements.Pop()));
+
+
+            }
+            if (op == "$or")
+            {
+                _elements.Push(BuildBinary(op, _elements.Pop(), _elements.Pop()));
+
+
+            }
+            if (op == "$eq")
+            {
+                _elements.Push(BuildBinary(op, _elements.Pop(), _elements.Pop()));
+
+
+            }
+            if (op == "from")
+            {
+                _elements.Push(BuildFrom(_elements.Pop()));
+
+
+            }
+            if (op == "select")
+            {
+                _elements.Push(BuildSelect(_elements.Pop()));
+
+
+            }
+        }
+    }
+}
